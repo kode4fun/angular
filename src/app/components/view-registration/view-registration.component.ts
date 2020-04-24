5e487ef5894d087005959533819c2b884da6634e
Naveen <ull.rac1@gmail.com>
2020-04-24 18:03:30 +0530

    Use Router to navigate to URL

--- src/app/components/view-registration/view-registration.component.ts
+++ src/app/components/view-registration/view-registration.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BikeService } from '../../services/bike.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-registration',
@@ -9,13 +9,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ViewRegistrationComponent implements OnInit {
   public bikeReg;
-  constructor(private bikeService: BikeService, private route: ActivatedRoute) { }
+  constructor(
+    private bikeService: BikeService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
-    let id: number = this.route.snapshot.params.id;
-    //id is defined as in routing.module.ts i.e, path: 'admin/view/:id'
+    //Snapshot method dont work when navigating to the same URL. (URL updates but view dont update.)
+    //When navigate back to the same component, angular reuses previously rendered component and dont update view. 
+    //let id = parseInt(this._route.snapshot.paramMap.get('id'))
 
-    this.getBikeReg(id); //subscribing to service
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id'));
+      //id is defined as in routing.module.ts i.e, path: 'admin/view/:id'
+      this.getBikeReg(id); //subscribing to service
+    });
   }
 
   getBikeReg(id: number) {
@@ -24,6 +32,11 @@ export class ViewRegistrationComponent implements OnInit {
       err => { console.error(err) },
       () => { console.log('bike ' + id + 'loaded') }
     );
+  }
+
+  goBackToAdmin() {
+    this.router.navigate(['../../'], { relativeTo: this.route });
+    // ../ go back one step in url
   }
 
 }
